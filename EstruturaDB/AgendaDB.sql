7a9164c018d49847cef5705caa653fdc3b8b16bc
AliciaPavao <aliciapavao369@gmail.com>
2024-11-19 11:32:05 -0300

    ATT bcd

--- EstruturaDB/AgendaDB.sql
+++ EstruturaDB/AgendaDB.sql
@@ -1,3 +1,10 @@
+-- Código para criar um usuario (@ no inicio para poder fazer em linhas separadas, $ no inicio para puxar as variaveis (itens em {}))
+-- @$"CREATE USER '{usuario}'@'%' IDENTIFIED BY '{senha}';
+
+-- Código para GARANTIR privilegios a algum usuario
+-- GRANT SELECT, INSERT, UPDATE ON *.* TO '{usuario}'@'%'; 
+
+
 CREATE DATABASE dbAgenda;
 
 USE dbAgenda;
@@ -13,5 +20,18 @@ select * from tbUsuarios;
 
 CREATE TABLE TbCategoria (
 	categorias VARCHAR(40) NOT NULL,
-    cod_categoria INT AUTO_INCREMENT primary key
+    cod_categoria INT AUTO_INCREMENT primary key,
+    usuario varchar(20)
     );
+
+DELIMITER $$
+CREATE TRIGGER 	TrIncertcategoria
+BEFORE
+INSERT
+ON Tbcategoria
+FOR EACH ROW
+BEGIN
+	SET NEW.usuario = CURRENT_USER();
+END$$
+    
+select * from mysql.user;    
